36d8252ce2ce2e51e53662823f65ed71e0577fd4
Olzah <oleh.zaho@gmail.com>
2023-07-02 16:35:46 +0200

    [INZ-016] feat/fix: add opinion views to survey, fix math calc current rate
    add opinion views to survey when is clicked on thumb button
    fix math calc current rate
    fix divided by zero error, when survey doesn't have any rate yet

--- addons/elearnmacomod/models/survey_survey.py
+++ addons/elearnmacomod/models/survey_survey.py
@@ -4,13 +4,19 @@ from odoo import api, fields, models
 class SurveySurvey(models.Model):
     _inherit = "survey.survey"
 
-    success_rating_count = fields.Integer("Success", compute="_compute_survey_rating_statistic")
+    success_rating_count = fields.Float("Success", compute="_compute_survey_rating_statistic")
 
     def _compute_survey_rating_statistic(self):
         self.success_rating_count = 0
         rating_records = self.env['survey.ratings'].search([("survey_survey_id", "=", self.id)])
-        for rec in rating_records:
-            self.success_rating_count += int(rec.user_rating)
-        return round(self.success_rating_count / len(rating_records), 1)
+        if rating_records:
+            for rec in rating_records:
+                self.success_rating_count += int(rec.user_rating)
+            self.success_rating_count = round(self.success_rating_count / len(rating_records), 1)
+
     def action_set_ratings_2(self):
-        print('Hello World from survey.survey!!!')
+        action = self.env['ir.actions.act_window']._for_xml_id('elearnmacomod.survey_ratings_base_menu_action')
+        ctx = dict(self.env.context)
+        ctx.update({'search_default_survey_survey_id': self.id})
+        action['context'] = ctx
+        return action
