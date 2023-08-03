3169713391fc4d14e0a72f44e000d2912816ed67
Patryk Stefan Pniewski <patryk.s.pniewski@gmail.com>
2023-08-03 12:31:56 +0200

    [INZ-050] Fixed tests and added a new one

--- addons/elearnmacomod/tests/test_rating.py
+++ addons/elearnmacomod/tests/test_rating.py
@@ -1,4 +1,5 @@
 # coding: utf-8
+import psycopg2.errors
 from odoo.tests import common, tagged
 
 
@@ -7,31 +8,70 @@ from odoo.tests import common, tagged
 class TestSurveyModel(common.TransactionCase):
     def test_calculate_rating(self):
         current_survey = self.env['survey.survey'].browse(5)
-        current_survey._compute_survey_rating_statistic()
-        current_survey.success_rating_count
-        self.assertEqual(current_survey.success_rating_count, 2.3)
+        test_ratings = [
+            {
+                'reviewer_id': 1,
+                'user_rating': '1',
+                'survey_survey_id': current_survey.id,
+                'opinion': "Some Example text"
+            },
+            {
+                'reviewer_id': 2,
+                'user_rating': '2',
+                'survey_survey_id': current_survey.id,
+                'opinion': "Some Example text"
+            },
+            {
+                'reviewer_id': 3,
+                'user_rating': '3',
+                'survey_survey_id': current_survey.id,
+                'opinion': "Some Example text"
+            },
+            {
+                'reviewer_id': 4,
+                'user_rating': '4',
+                'survey_survey_id': current_survey.id,
+                'opinion': "Some Example text"
+            },
+        ]
+        for rating in test_ratings:
+            self.assertTrue(self.env['survey.ratings'].create(rating))
+        self.assertEqual(current_survey.success_rating_count, 2.5)
 
     def test_create_survey_rating(self):
         current_survey = self.env['survey.survey'].browse(5)
         set_rating = {
             'reviewer_id': 3,
-            'user_rating': 12,
+            'user_rating': '3',
             'survey_survey_id': current_survey.id,
             'opinion': "Some Example text"
         }
-        survey = self.env['survey.survey'].create(set_rating)
+        survey = self.env['survey.ratings'].create(set_rating)
         self.assertTrue(survey)
 
-    def test_is_user_create_survey_rating_without_complete_survey(self):
+    def test_rate_survey_multiple_times(self):
         current_survey = self.env['survey.survey'].browse(5)
         set_rating = {
-            'reviewer_id': 13,
-            'user_rating': 12,
+            'reviewer_id': 3,
+            'user_rating': '3',
             'survey_survey_id': current_survey.id,
             'opinion': "Some Example text"
         }
-        survey = self.env['survey.survey'].create(set_rating)
-        self.assertTrue(survey)
+        rating_1 = self.env['survey.ratings'].create(set_rating)
+        self.assertTrue(rating_1)
+        with self.assertRaises(psycopg2.errors.UniqueViolation):
+            rating_2 = self.env['survey.ratings'].create(set_rating)
+
+    # def test_is_user_create_survey_rating_without_complete_survey(self):
+    #     current_survey = self.env['survey.survey'].browse(5)
+    #     set_rating = {
+    #         'reviewer_id': 13,
+    #         'user_rating': '3',
+    #         'survey_survey_id': current_survey.id,
+    #         'opinion': "Some Example text"
+    #     }
+    #     survey = self.env['survey.ratings'].create(set_rating)
+    #     self.assertFalse(survey)
 
     def test_create_survey_rating_without_rate(self):
         current_survey = self.env['survey.survey'].browse(5)
@@ -40,5 +80,5 @@ class TestSurveyModel(common.TransactionCase):
             'survey_survey_id': current_survey.id,
             'opinion': "Some Example text"
         }
-        survey = self.env['survey.survey'].create(set_rating)
-        self.assertTrue(survey)
+        survey = self.env['survey.ratings'].create(set_rating)
+        self.assertFalse(survey)
